5e398a4005c3c77e3c63f6ae3617652f7425560d
M1CROSLAN-CORP <anthonycaicedocongolino444@gmail.com>
2025-04-02 04:35:40 -0500

    Tercer Dia Adelanto parcial

--- Primer_Parcial.cpp
+++ Primer_Parcial.cpp
@@ -1,18 +1,20 @@
 // ESTUDIANTE * ANTHONY CAICEDO CONGOLINO
 
 #include <iostream>
+#include <string.h>
 using namespace std;
 
-//Declaracion Estructura JUGADORES
+//Declaracion Estructura JUGADORES TIPO FIFO
 struct jugadores {
     int id_cedula,edad;
     char nombre[50];
     char deporte[50], genero;
     struct jugadores *sig;//NULL
 };
-struct jugadores *cab, *aux, *ultimo;
+struct jugadores *cab, *aux, *ultimo, *temp;
+// FIN DE LA ESTRUCTURA
 
-
+//DECLARACION ESTRUCTURA PARTICIPACION TIPO LIFO
 struct Participacion {
     int id_cedula_P,edad_P;
     char nombre_P[50];
@@ -20,8 +22,9 @@ struct Participacion {
     struct Participacion *sig;//NULL
 };
 struct Participacion *toP, *auxP, *aux2P;
+//FIN DE LA ESTRUCTURA
 
-int CantidadJugadores=0,contarP=0,contarJ=0;
+int CantidadJugadores=0, contarP=0, contarJ=0;
 
 int RegistrarJugador(){    
     aux = ((struct jugadores *) malloc(sizeof(struct jugadores)));
@@ -42,18 +45,15 @@ int RegistrarJugador(){
         }else if (opc==2){
          aux->genero = 'M';
         }else{
-            cout<<"Marcacion Incorrecta"<<endl;
+            cout<<"Marcacion Incorrecta"<<endl<<endl;
         }       
     }while(opc<1 || opc>2);
 
     cout<<"Registre La Edad Del Jugador "<<endl;
     cin>>aux->edad;
 
-    cout<<"Registre El Deporte a Participar ";
-    cin>>aux->deporte;
-
-    cout<<"## El  Jugador "<< aux->nombre <<" Registrado Correctamente "<<endl<<endl;
-    /*
+    cout<<"Registre El Deporte a Participar "<<endl;
+   
     do {
         cout<<"1. Futbol "<<endl;
         cout<<"2. Baloncesto "<<endl;
@@ -62,15 +62,16 @@ int RegistrarJugador(){
         cin>>opc;
         switch (opc)
         {
-            case 1: aux->deporte = "Futbol"; break;
-            case 2: aux->deporte = "Baloncesto"; break;
-            case 3: aux->deporte = "Atletismo"; break;
-            case 4: aux->deporte = "Patinaje"; break;
+            case 1: strcpy(aux->deporte,"Futbol"); break;
+            case 2: strcpy(aux->deporte,"Baloncesto"); break;
+            case 3: strcpy(aux->deporte,"Atletismo"); break;
+            case 4: strcpy(aux->deporte,"Patinaje"); break;
             default: cout<<"Opcion Incorrecta Marque una Opcion De 1 Hasta 4 "<<endl;
         }
         
     }while(opc<1 || opc>4);
-    */
+    
+    cout<<"## El  Jugador "<< aux->nombre <<" Registrado Correctamente "<<endl<<endl;
 
     aux->sig = NULL;
     if(cab==NULL){
@@ -99,7 +100,7 @@ int MostrarCantidadJugadores(){
 }
 
 int MostrarJugadores(){
-     if(contarJ!=0){ 
+     if(contarJ>0){ 
         int cont=1;
         for(aux=cab; aux!=NULL; aux=aux->sig){
             cout<< "      Jugadores Inscritos  # "  << cont   <<endl;
@@ -119,50 +120,77 @@ int MostrarJugadores(){
 }
 
 int MostrarHistorial(){
-    if(CantidadJugadores!=0){
+    if(contarP>0){
         for(auxP=toP; auxP!=NULL; auxP=auxP->sig){
-            cout<< "                Historial Participacion Jugadores  # "<< CantidadJugadores <<endl;
-            cout<< "#######################################################################"<<endl;
-            cout<< " Cedula Del Jugador: = " <<auxP->id_cedula_P << " = Ubicacion En Memoria = "<< auxP <<endl;
-            cout<< " Nombre Del Jugador: = " <<auxP->nombre_P << " = Ubicacion En Memoria = "<< auxP <<endl;
-            cout<< " Edad Del Jugador: = " <<auxP->edad_P << " = Ubicacion En Memoria = "<< auxP <<endl;
-            cout<< " Genero Del jugador: = " <<auxP->genero_P << " = Ubicacion En Memoria = "<< auxP <<endl;
-            cout<< " Su Deporte Es: = " <<auxP->deporte_P << " = Ubicacion En Memoria = "<< auxP <<endl;
-            cout<< "#######################################################################"<<endl;
-            contarP++;       
+            cout<< "    Historial Participacion Jugadores  # "<< contarP <<endl;
+            cout<< "####################################################"<<endl;
+            cout<< " Cedula Del Jugador: = " << auxP->id_cedula_P   <<endl;
+            cout<< " Nombre Del Jugador: = " << auxP->nombre_P      <<endl;
+            cout<< " Edad Del Jugador: = " << auxP->edad_P          <<endl;
+            cout<< " Genero Del jugador: = "<<  auxP->genero_P      <<endl;
+            cout<< " Su Deporte Es: = " << auxP->deporte_P          <<endl;
+            cout<< "#####################################################"<<endl;      
         }
     }else{
         cout<< " No Se ha Registrado Participacion De Jugadores Al Momento "<<endl<<endl;
     }
 }
 
-int Participacion(){
-   auxP = ((struct Participacion *) malloc(sizeof(struct Participacion)));
+int Participacion(){   
+   int opc=0;
    for(aux=cab; aux!=NULL; aux=aux->sig){
-        if(aux!=NULL){
+        if(aux==cab && opc==0){
             if(aux->edad>15){
+                auxP = ((struct Participacion *) malloc(sizeof(struct Participacion)));
+                strcpy(auxP->nombre_P, aux->nombre);
+                strcpy(auxP->deporte_P, aux->deporte);
                 auxP->id_cedula_P = aux->id_cedula;
-                //auxP->nombre_P = aux->nombre;
                 auxP->edad_P = aux->edad;
                 auxP->genero_P = aux->genero;
-                //auxP->deporte_P = aux->deporte;
-             cout<< " El Jugador Ha Quedado Registrado Para participar "<<endl<<endl;
+             cout<< " El Jugador " << aux->nombre <<" Ha Quedado Registrado Para participar "<<endl<<endl;
              CantidadJugadores--;
              contarP++;
+
+              if(toP==NULL){
+                toP = auxP;
+                toP->sig = NULL;
+              } else {
+                        auxP->sig = toP;
+                        toP = auxP;
+                    }
+                auxP = NULL;
+                free(auxP);
+
+             //Desencolar De En La Lista FiFO EL JUGADOR QUE ENTRO A PARTICIPAR
+             temp = cab;
+             cab = cab->sig;
+             free(temp);     
+             opc=1;
            }else{
-              cout<<" No Cumple Con La Edad Debe Tener Mas De 15 Años "<<endl<<endl;
+              cout<<" No Cumple Con La Edad Debe Tener Mas De 15 A\u00F1os "<<endl<<endl;
+              cout<<" Se Eliminara Este Jugador De La Lista FIFO ? "<<endl;
+              cout<<" 1: SI "<<endl;
+              cout<<" 2: NO "<<endl;
+              cin>>opc;
+
+              if(opc==1){
+                 cout<<" EL Jugador De Nombre " << aux->nombre << " Quedo Eliminado Por Tener " << aux->edad << " Años "<<endl;
+                 //Desencolar De En La Lista FIFO POR SER MENOR DE EDAD
+                 temp = cab;
+                 cab = cab->sig;
+                 free(temp);
+                 CantidadJugadores--;
+              }else{
+                 cout<<" No Se Elimino El Jugador "<<endl;
+                 opc=1;
+              }
            }
         }
     }
-    if(toP==NULL){
-        toP = auxP;
-        toP->sig = NULL;
-    } else {
-        auxP->sig = toP;
-        toP = auxP;
-    }
-    auxP = NULL;
-    free(auxP);
+    
+    
+
+    
     
 }
 
@@ -172,7 +200,7 @@ int main(){
         cout<<"1. Agregar Jugador a Cola "<<endl;
         cout<<"2. Mostrar Jugadores En Espera "<<endl;
         cout<<"3. Contar Jugadores En Espera "<<endl;
-        cout<<"4. Permitir participacion del siguiente jugador "<<endl;
+        cout<<"4. Permitir participacion Segun El Orden En La Fila "<<endl;
         cout<<"5. Mostrar Historial De Participacion "<<endl;
         cout<<"6. Deshacer Ultima Participacion"<<endl;
         cout<<"7. Salir"<<endl;
