f75a8253c4b453af9908abcc0b081bd242692281
M1CROSLAN-CORP <anthonycaicedocongolino444@gmail.com>
2025-05-02 02:48:39 -0500

    Funcion Para buscar Por Genero

--- Taller_Arboles/Taller_2.cpp
+++ Taller_Arboles/Taller_2.cpp
@@ -13,10 +13,11 @@ struct cinemania {
 };
 
 struct cinemania *raiz, *aux;
-char BusquedaPorNombre[50];
+char BusquedaPorNombre[50],BusquedaPorGenero[50];
 
 void posicionar(struct cinemania *nuevaraiz){
-    if(aux->ano_realizado<nuevaraiz->ano_realizado || aux->ano_realizado==nuevaraiz->ano_realizado){
+    //Si el Nuevo Año ingresado es menor o igual al año ya registrado va para la izquierda
+    if(aux->ano_realizado<nuevaraiz->ano_realizado || aux->ano_realizado==nuevaraiz->ano_realizado){ 
         if(nuevaraiz->izq==NULL){
             nuevaraiz->izq = aux;
         } else {
@@ -42,10 +43,10 @@ int registrar(){
     cin.ignore();  // Limpiar el buffer antes de usar getline
     cin.getline(aux->nombre_pelicula, 50);  // Leer una línea completa, hasta 50 caracteres
 
-    cout<<" Registre El Año En Que Se Realizo "<<endl;
+    cout<<"Registre El Año En Que Se Realizo "<<endl;
     cin>>aux->ano_realizado;
     
-    cout<<" Registre El Genero De La Pelicula "<<endl;    
+    cout<<"Registre El Genero De La Pelicula "<<endl;    
     do {
         cout<<"1. Accion "<<endl;
         cout<<"2. Romance "<<endl;
@@ -54,16 +55,16 @@ int registrar(){
         cin>>opc;
         switch (opc)
         {
-            case 2: strcpy(aux->genero,"Accion"); break;
-            case 3: strcpy(aux->genero,"Romance"); break;
-            case 1: strcpy(aux->genero,"Terror"); break;
+            case 1: strcpy(aux->genero,"Accion"); break;
+            case 2: strcpy(aux->genero,"Romance"); break;
+            case 3: strcpy(aux->genero,"Terror"); break;
             case 4: strcpy(aux->genero,"Comedia"); break;
             default: cout<<"Opcion Incorrecta Marque una Opcion De 1 Hasta 4 "<<endl;
         }
         
     }while(opc<1 || opc>4);
 
-    cout<<" Registre La totalidad De Dinero Recaudado ";
+    cout<<"Registre La totalidad De Dinero Recaudado ";
     cin>>aux->dinero_recaudado;
 
 
@@ -103,45 +104,80 @@ int mostrarInOrden(struct cinemania *nuevaraiz){
     }
 }
 
-int BuscarNombrePelicula(struct cinemania *buscar_raiz){
-    if(strcmp(BusquedaPorNombre, "") == 0){
-      cout<<"Ingrese el Nombre De la Pelicula A Buscar "<<endl;
-      cin.ignore();  // Limpiar el buffer antes de usar getline
-      cin.getline(BusquedaPorNombre, 50);  // Leer una línea completa, hasta 50 caracteres
-    }                     
-                      
-    
+int BuscarNombrePelicula(struct cinemania *buscar_raiz){                
     if(buscar_raiz!=NULL){
         if(strcmp(BusquedaPorNombre, buscar_raiz->nombre_pelicula) == 0){
             cout<<"Nombre Pelicula = "<<buscar_raiz->nombre_pelicula<<endl;
             cout<<"Ano De Estreno = "<<buscar_raiz->ano_realizado<<endl<<endl;
-            strcpy(BusquedaPorNombre,"");
+            BuscarNombrePelicula(buscar_raiz->izq);
+            BuscarNombrePelicula(buscar_raiz->der);
         }else{      
             BuscarNombrePelicula(buscar_raiz->izq);
             BuscarNombrePelicula(buscar_raiz->der);
         } 
-    }
-
-        
+    }       
 }
 
+int BuscarPeliculasPorGenero(struct cinemania *buscar_raiz){
+    if(buscar_raiz!=NULL){   
+        if(strcmp(BusquedaPorGenero, buscar_raiz->genero)==0){
+            cout<<"Nombre Pelicula = "<<buscar_raiz->nombre_pelicula<<endl;
+            cout<<"Ano De Estreno = "<<buscar_raiz->ano_realizado<<endl;
+            cout<<"Genero Pelicula = "<<buscar_raiz->genero<<endl<<endl;
+            BuscarPeliculasPorGenero(buscar_raiz->izq);
+            BuscarPeliculasPorGenero(buscar_raiz->der);
+        }else{      
+             BuscarPeliculasPorGenero(buscar_raiz->izq);
+             BuscarPeliculasPorGenero(buscar_raiz->der);
+        }       
+    }
+}
+
+int fracasostaquilleros(struct cinemania *buscar_raiz){
+  cout<<"Los Tres Ultimos Fracasos Taquilleros"<<endl;
+}
 
 int main(){
     int opcion;
     do{
-        cout<<"1. Registrar Una Nueva Pelicula"<<endl;
-        cout<<"2. Buscar Una Pelicula Por Nombre"<<endl;
-        cout<<"3. Buscar Peliculas Por Genero"<<endl;
-        cout<<"4. Mostrar los 3 Fracasos Taquilleros"<<endl;
-        cout<<"5. Mostrar Arbol En PRE-orden"<<endl;
-        cout<<"6. Mostrar Arbol En IN-orden"<<endl;
-        cout<<"7. Mostrar Arbol En POST-orden"<<endl;
-        cout<<"8. Salir Del Sistema"<<endl;
-        cout<<"Su opcion es:"<<endl;
+        cout<<"1. Registrar Una Nueva Pelicula "<<endl;
+        cout<<"2. Buscar Una Pelicula Por Nombre "<<endl;
+        cout<<"3. Buscar Peliculas Por Genero "<<endl;
+        cout<<"4. Mostrar los 3 Fracasos Taquilleros "<<endl;
+        cout<<"5. Mostrar Arbol En PRE-orden "<<endl;
+        cout<<"6. Mostrar Arbol En IN-orden "<<endl;
+        cout<<"7. Mostrar Arbol En POST-orden "<<endl;
+        cout<<"8. Salir Del Sistema "<<endl;
+        cout<<"Su opcion es: "<<endl;
         cin>>opcion;
         switch(opcion){
             case 1: registrar(); break;
-            case 2: BuscarNombrePelicula(raiz); break;
+            case 2:{
+                       strcpy(BusquedaPorNombre,"");
+                       cout<<"Ingrese el Nombre De la Pelicula A Buscar "<<endl;
+                       cin.ignore();
+                       cin.getline(BusquedaPorNombre, 50);  // Leer una línea completa, hasta 50 caracteres
+                       BuscarNombrePelicula(raiz); break;} 
+            case 3:{
+                        int opc=0;
+                        cout<<"Seleccione El Genero Para Mostrar Peliculas"<<endl;    
+                        do {
+                            cout<<"1. Accion "<<endl;
+                            cout<<"2. Romance "<<endl;
+                            cout<<"3. Terror"<<endl;
+                            cout<<"4. Comedia"<<endl;
+                            cin>>opc;
+                            switch (opc)
+                            {
+                                case 1: strcpy(BusquedaPorGenero,"Accion"); break;
+                                case 2: strcpy(BusquedaPorGenero,"Romance"); break;
+                                case 3: strcpy(BusquedaPorGenero,"Terror"); break;
+                                case 4: strcpy(BusquedaPorGenero,"Comedia"); break;
+                                default: cout<<"Opcion Incorrecta Marque una Opcion De 1 Hasta 4 "<<endl;
+                            }                            
+                        }while(opc<1 || opc>4);
+                        BuscarPeliculasPorGenero(raiz);break;}
+            case 4: fracasostaquilleros(raiz); break;            
             case 5:{ cout<<"Mostrando Contenido En PRE - Orden "<<endl; mostrarPreOrden(raiz); break; }
             case 6:{ cout<<"Mostrando Contenido En IN - Orden "<<endl; mostrarInOrden(raiz); break; }
             case 7:{ cout<<"Mostrando Contenido En POST - Orden "<<endl; mostrarPostOrden(raiz); break; }
